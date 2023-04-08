12ba70bd4f0c111c9f75ab5159dea52cba4b8e40
Semma Alfatlawi <semmaalfatlawi@lev-13380.apn.wlan.private.upenn.edu>
2023-04-08 14:55:55 -0400

    front page

--- client/src/pages/AlbumsPage.js
+++ client/src/pages/AlbumsPage.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Box, Container } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import './pages.css';
+import world from '../images/world.jpeg';
 
 const config = require('../config.json');
 
@@ -22,13 +24,63 @@ export default function AlbumsPage() {
   return (
     // TODO (TASK 22): replace the empty object {} in the Container's style property with flexFormat. Observe the change to the Albums page.
     // TODO (TASK 22): then uncomment the code to display the cover image and once again observe the change, i.e. what happens to the layout now that each album card has a fixed width?
-    <Container style={flexFormat}>
+    <Container style={{ backgroundImage: `url(${world})`, backgroundSize: "cover"}}>
+      
+      <h2 className="centered-h2"> TIME TO TRAVEL:&nbsp; 
+      </h2>
+      <h4 className="centered-h2">What are you searching for?&nbsp;
+      </h4>
+      <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}>
+      <Box
+          className="left-panel"
+          p={3}
+          m={2}
+          style={{ width: 300, height: 100, background: 'paleturquoise', borderRadius: '16px', border: '2px solid #000', textAlign: 'center' }}
+        >
+          Hotels
+         </Box>
+      <Box
+          className="middle-panel"
+          p={3}
+          m={2}
+          style={{ width: 300, height: 100, background: 'paleturquoise', borderRadius: '16px', border: '2px solid #000', textAlign: 'center' }}
+        >
+          Attractions
+         </Box>
+      <Box
+          className="right-panel"
+          p={3}
+          m={2}
+          style={{ width: 300, height: 100, background: 'paleturquoise', borderRadius: '16px', border: '2px solid #000', textAlign: 'center' }}
+        >
+          Restaurants
+         </Box>
+         </div>
+      <h2 className="centered-h2"> OR&nbsp; 
+      </h2>
+      <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}}>
+      <Box
+          p={3}
+          m={2}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',width: 300, height: 100, background: 'paleturquoise', borderRadius: '16px', border: '2px solid #000', textAlign: 'center' }}
+        >
+          Build an itinerary
+         </Box>
+         </div>
       {albums.map((album) =>
         <Box
           key={album.album_id}
           p={3}
           m={2}
-          style={{ background: 'white', borderRadius: '16px', border: '2px solid #000' }}
+          style={{ background: 'white', borderRadius: '16px', border: '2px solid #000'}}
         >
           {
           <img
